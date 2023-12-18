06efaa861969b03abe28bc7208b4df5a1e8c1958
mr_huang <mr_huang@163.com>
2023-12-18 11:53:05 +0800

    [修改] 将动态数组的API接口加上形参

--- dynamicArray/dynamicArray.h
+++ dynamicArray/dynamicArray.h
@@ -14,31 +14,31 @@ typedef struct dynamicArray
 
 /* API：appliction program interface */
 /* 动态数组初始化 */
-int dynamicArrayInit();
+int dynamicArrayInit(dynamicArray *pArray);
 
 /* 动态数组插入数据(默认插入到数组末尾) */
-int dynamicArrayInsertData();
+int dynamicArrayInsertData(dynamicArray *pArray, ELEMENTTYPE val);
 
 /* 动态数组插入数据在指定位置插入 */
-int dynamicArrayAppoitPosInserData();
+int dynamicArrayAppoitPosInserData(dynamicArray *pArry, int pos, ELEMENTTYPE val);
 
 /* 动态数组修改指定位置的数据 */
-int dynamicArrayModifAppointPosData();
+int dynamicArrayModifAppointPosData(dynamicArray *pArry, int pos, ELEMENTTYPE val);
 
 /* 动态数组的删除数据(默认下删除末尾的数据) */
-int dynamicArrayDeleteData();
+int dynamicArrayDeleteData(dynamicArray *pArray);
 
 /* 动态数组的指定位置删除 */
-int dynamicArrayDeleteAppointPosData();
+int dynamicArrayDeleteAppointPosData(dynamicArray *pArry, int pos);
 
 /* 动态数组的销毁 */
-int dynamicArrayDestory();
+int dynamicArrayDestory(dynamicArray *pArray);
 
 /* 获取数组的大小 */
-int dynamicArrayGetSize();
+int dynamicArrayGetSize(dynamicArray *pArray, int *pSize);
 
 /* 获取数组的容量 */
-int dynamicArrayGetCapacity();
+int dynamicArrayGetCapacity(dynamicArray *pArray, int *pSize);
 
 #endif //__DYNAMIC_ARRAY_H_
 
