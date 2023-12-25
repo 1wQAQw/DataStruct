c69f4c73d414ff55ff22fa7a036e3f200803961a
mr_huang <mr_huang@163.com>
2023-12-25 15:25:55 +0800

    [修改] 完成二叉树的初始化接口

--- BinarySearchTree/binarySearchTree.c
+++ BinarySearchTree/binarySearchTree.c
@@ -1,8 +1,54 @@
 #include "binarySearchTree.h"
+#include <stdio.h>
+#include <string.h>
 
+/* 状态码 */
+enum STATUS_CODE
+{
+    NOT_FIND = -1,
+    ON_SUCCESS,
+    NULL_PTR,
+    MALLOC_ERROR,
+    INVALID_ACCESS,
+};
 
-int main()
+/* 二叉搜索树的初始化 */
+int bianrySearchTreeInit(BinarySearchTree **pBstree)
+{
+    int ret = 0;
+    BinarySearchTree * bstree = (BinarySearchTree *)malloc(sizeof(BinarySearchTree) * 1);
+    if(bstree == NULL)
+    {
+        return MALLOC_ERROR;
+    }
+    /* 清除脏数据 */
+    memset(bstree, 0, sizeof(BinarySearchTree));
+    /* 初始化树 */
+    {
+        bstree->root = NULL;
+        bstree->size = 0;
+
+    }
+
+    #if 1
+    bstree->root = (BSTreeNode *)malloc(sizeof(BSTreeNode) * 1);
+    /* 清除脏数据 */
+    memset(bstree->root, 0, sizeof(BSTreeNode) * 1);
+    /* 初始化根节点 */
+    {
+        bstree->root->data = 0;
+        bstree->root->left = NULL;
+        bstree->root->right = NULL;
+        bstree->root->parent = NULL;
+    }
+    #endif
+
+    return ret;
+}
+
+/* 二叉搜索树的插入 */
+int bianrySearchTreeInsert(BinarySearchTree **pBstree, ELEMENTTYPE val)
 {
 
-    return 0;
 }
+
